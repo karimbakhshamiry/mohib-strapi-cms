5195f4a49218745810129870746776e85c71534d
Karim Bakhsh Amiry <karimbakhshamiry.123@gmail.com>
2025-05-28 03:54:52 +0500

    configured s3 as an upload provider

--- config/plugins.ts
+++ config/plugins.ts
@@ -1,7 +1,25 @@
 module.exports = ({ env }) => ({
-  "strapi-plugin-populate-deep": {
+  upload: {
     config: {
-      defaultDepth: 3, // Default is 5
+      provider: "aws-s3",
+      providerOptions: {
+        s3Options: {
+          credentials: {
+            accessKeyId: env("AWS_ACCESS_KEY_ID"),
+            secretAccessKey: env("AWS_ACCESS_SECRET"),
+          },
+          region: env("AWS_REGION"),
+          params: {
+            ACL: env("AWS_ACL", "public-read"),
+            Bucket: env("AWS_BUCKET"),
+          },
+        },
+      },
+      actionOptions: {
+        upload: {},
+        uploadStream: {},
+        delete: {},
+      },
     },
   },
 });
